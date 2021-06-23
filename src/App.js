88fd39ef40a224c3a7854abfdc6e25565be49f40
weichieh123 <weichiehhuang@gapp.nthu.edu.tw>
2021-06-23 13:34:25 +0800

    0623 覆蓋bootstrap 測試

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React  from 'react'
+import React from 'react'
 
 function App() {
-
-  return(
+  return (
     <>
-      
+  
+  <button type="button" className="btn btn-primary">Primary</button>
+  <button type="button" className="btn btn-secondary">Secondary</button>
+  <button type="button" className="btn btn-success">Success</button>
+  <button type="button" className="btn btn-danger">Danger</button>
+  <button type="button" className="btn btn-warning">Warning</button>
+  <button type="button" className="btn btn-info">Info</button>
+
+
     </>
   )
 }
